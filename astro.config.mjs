af0fd8d390367fbba1526d9fd408ddb5672862f4
Tolumi Adamson <tolumi@kenshomedia.com>
2023-12-18 22:46:39 +0000

    Docker FIle, DockerIgnore, Node adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import "dotenv/config";
-
 import svelte from "@astrojs/svelte";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   vite: {
     define: {
-      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString()),
-    },
+      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString())
+    }
   },
   image: {
     domains: ["amazonaws.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
