d2356316a146b4bda9c3057be28966a8ab2b3206
Ikenna <onyejiaka.samuel@gmail.com>
2024-01-29 13:30:24 +0100

    WIP  - Software Slot Machine Page

---
+++ qs/ssm-games.ts
@@ -0,0 +1,98 @@
+export const gamesQs = (
+  slug: string = "",
+  sort: string = "",
+  page: number = 1
+) => ({
+  fields: ["title", "slug", "ratingAvg", "ratingCount", "publishedAt"],
+  populate: {
+    provider: {
+      fields: ["title", "slug"],
+      populate: {
+        images: {
+          fields: ["url"],
+        },
+      },
+    },
+    categories: {
+      fields: ["title"],
+    },
+    images: {
+      fields: ["url"],
+    },
+  },
+  pagination: {
+    page: page || 1,
+    pageSize: 18,
+    withCount: true,
+  },
+  filters: {
+    provider: { slug: { $eq: slug } },
+  },
+  sort: sort,
+});
+
+export const slotProvidersQs = (slug: string = "") => ({
+  fields: [
+    "title",
+    "heading",
+    "createdAt",
+    "content1",
+    "content2",
+    "content3",
+    "showGameFilterPanel",
+  ],
+  populate: {
+    relatedCasinos: {
+      fields: ["title", "slug", "ratingAvg", "ratingCount"],
+      populate: {
+        images: {
+          fields: ["url"],
+        },
+        casinoBonus: {
+          fields: ["bonusUrl", "bonusLabel", "bonusCode"],
+        },
+        noDepositSection: {
+          fields: ["bonusAmount", "termsConditions"],
+        },
+        freeSpinsSection: {
+          fields: ["bonusAmount", "termsConditions"],
+        },
+        bonusSection: {
+          fields: ["bonusAmount", "termsConditions", "cashBack", "freeSpin"],
+        },
+        termsAndConditions: {
+          fields: ["copy", "gambleResponsibly"],
+        },
+        providers: {
+          fields: ["title"],
+          pagination: {
+            page: 1,
+            pageSize: 100,
+          },
+          populate: {
+            images: {
+              fields: ["url"],
+            },
+          },
+        },
+      },
+    },
+    faqs: {
+      fields: ["question", "answer"],
+    },
+    seo: {
+      fields: ["metaTitle", "metaDescription"],
+    },
+    IntroductionWithImage: {
+      fields: ["heading", "introduction"],
+      populate: {
+        image: {
+          fields: ["url", "mime"],
+        },
+      },
+    },
+  },
+  filters: {
+    slug: { $eq: slug },
+  },
+});
