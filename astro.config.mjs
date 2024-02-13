f177911ed3296f3eaef7db09e1616825fd5ad30b
Tolumi Adamson <tolumi@kenshomedia.com>
2024-02-13 10:09:07 +0000

    Fixed translationdata error, Astro Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,49 +5,38 @@ import svelte from "@astrojs/svelte";
 import node from "@astrojs/node";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
-
+import compress from "astro-compress";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), compress()],
   vite: {
-    plugins: [
-      Icons({
-        compiler: "svelte",
-        customCollections: {
-          "kensho-icons": FileSystemIconLoader("./src/icons", (svg) =>
-            svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-          "kensho-dashboard-icons": FileSystemIconLoader(
-            "./src/icons/dashboard",
-            (svg) => svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-        },
-      }),
-      Icons({
-        compiler: "astro",
-        customCollections: {
-          "kensho-icons": FileSystemIconLoader("./src/icons", (svg) =>
-            svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-        },
-      }),
-    ],
+    plugins: [Icons({
+      compiler: "svelte",
+      customCollections: {
+        "kensho-icons": FileSystemIconLoader("./src/icons", svg => svg.replace(/^<svg /, '<svg fill="currentColor" ')),
+        "kensho-dashboard-icons": FileSystemIconLoader("./src/icons/dashboard", svg => svg.replace(/^<svg /, '<svg fill="currentColor" '))
+      }
+    }), Icons({
+      compiler: "astro",
+      customCollections: {
+        "kensho-icons": FileSystemIconLoader("./src/icons", svg => svg.replace(/^<svg /, '<svg fill="currentColor" '))
+      }
+    })],
     define: {
-      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString()),
-    },
+      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString())
+    }
   },
   image: {
     domains: ["amazonaws.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   output: "server",
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
