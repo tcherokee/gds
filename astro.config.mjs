68ee72dca3290817d65f0cc58f2322d1d5971964
Tolumi Adamson <tolumi@kenshomedia.com>
2024-02-19 12:32:13 +0000

    Added Partytown, Fixed JS Errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,52 +6,41 @@ import node from "@astrojs/node";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_FULL_URL,
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [tailwind(), svelte(), sitemap(), partytown()],
   base: process.env.PUBLIC_SITE_ID === "gds" ? "/it" : "",
   trailingSlash: "always",
   vite: {
-    plugins: [
-      Icons({
-        compiler: "svelte",
-        customCollections: {
-          "kensho-icons": FileSystemIconLoader("./src/icons", (svg) =>
-            svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-          "kensho-dashboard-icons": FileSystemIconLoader(
-            "./src/icons/dashboard",
-            (svg) => svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-        },
-      }),
-      Icons({
-        compiler: "astro",
-        customCollections: {
-          "kensho-icons": FileSystemIconLoader("./src/icons", (svg) =>
-            svg.replace(/^<svg /, '<svg fill="currentColor" ')
-          ),
-        },
-      }),
-    ],
+    plugins: [Icons({
+      compiler: "svelte",
+      customCollections: {
+        "kensho-icons": FileSystemIconLoader("./src/icons", svg => svg.replace(/^<svg /, '<svg fill="currentColor" ')),
+        "kensho-dashboard-icons": FileSystemIconLoader("./src/icons/dashboard", svg => svg.replace(/^<svg /, '<svg fill="currentColor" '))
+      }
+    }), Icons({
+      compiler: "astro",
+      customCollections: {
+        "kensho-icons": FileSystemIconLoader("./src/icons", svg => svg.replace(/^<svg /, '<svg fill="currentColor" '))
+      }
+    })],
     define: {
-      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString()),
-    },
+      "process.env.VITE_BUILD_TIME": JSON.stringify(new Date().toISOString())
+    }
   },
   image: {
     domains: ["amazonaws.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   output: "server",
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
