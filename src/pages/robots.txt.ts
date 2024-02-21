355c578e034295bd19f42f0aaa20cb3f97c0f292
Tolumi Adamson <tolumi@kenshomedia.com>
2024-02-21 16:37:21 +0000

    Robots.txt File

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,55 @@
+import type { APIRoute } from "astro";
+
+console.log(import.meta.env.NODE_ENV);
+
+const rootURL = () => {
+    if (import.meta.env.NODE_ENV === 'production') {
+        return `Allow: /`
+    } else {
+        return `Disallow: /`
+    }
+
+}
+const customURLS = () => {
+        if (import.meta.env.SITE_ID === 'gds') {
+            return `
+                Disallow: /it-IT/
+                Disallow: /es-MX/
+                Disallow: /es-CO/
+                Disallow: /en/
+                Disallow: /it/slots/
+                Disallow: /it/slot-games/
+                Disallow: /it/slot-casinos/snai-casino/p*
+                Disallow: /it/slot-casinos/betway/p*
+                Disallow: /it/slot-casinos/goldbet/p*
+                Disallow: /it/slot-casinos/888-casino/p*
+                Disallow: /it/slot-machine/piu-giocate/p*
+                Disallow: /it/casino-slots/voglia-di-vincere-casino/p*
+                Disallow: /it/casino-slots/betflag-casino/p*
+                Disallow: /it/casino-slots/admiral-yes-casino/p*
+                Disallow: /it/casino-slots/admiral-yes-casino/p*
+                Disallow: /it/slot-machine/migliori/p*
+                Disallow: /it/slot-software/p*
+                Disallow: /it/slot-casinos/slotyes/p*
+                Disallow: /it/giri-gratis/p*
+                Disallow: /it/casino/recensione/slot-machines/
+                Disallow: /it/software-slot-machine/slot-machines/
+            `
+        }
+}
+
+const robotsTxt = `
+User-agent: *
+${rootURL().trim()}
+${import.meta.env.NODE_ENV === 'production' ? customURLS()?.trim() : ''}
+
+Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
+`.trim();
+
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+};
