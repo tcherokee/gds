6ea29801993a0bfb4c004ec53d5290b8b1dc2c24
Tolumi Adamson <tolumi@kenshomedia.com>
2024-03-22 11:46:25 +0000

    Set Up Matomo Tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,29 @@ import node from "@astrojs/node";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
 import sitemap from "@astrojs/sitemap";
+import matomo from "astro-matomo";
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_FULL_URL,
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    sitemap(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://analytics.kenshomedia.com/",
+      setCookieDomain: "*.kenshomedia.com",
+      trackerUrl: "js/", // defaults to matomo.php
+      srcUrl: "js/", // defaults to matomo.js
+      siteId: import.meta.env.MATOMO_SITE_ID,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+    }),
+  ],
   base: process.env.PUBLIC_SITE_ID === "gds" ? "/it" : "",
   trailingSlash: "always",
   vite: {
