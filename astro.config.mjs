7cb7b33b93d06cfe9b6e806d6ce551ba37d4e4a6
Tolumi Adamson <tolumi@kenshomedia.com>
2024-05-15 13:28:54 +0100

    Adding Swetrix to Main Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import node from "@astrojs/node";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
 import sitemap from "@astrojs/sitemap";
-import matomo from "astro-matomo";
 
 dotenv.config();
 
@@ -15,22 +14,7 @@ console.log("enabled", process.env.IS_PROD === "true", process.env.IS_PROD);
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_FULL_URL,
-  integrations: [
-    tailwind(),
-    svelte(),
-    sitemap(),
-    matomo({
-      enabled: process.env.IS_PROD === "true", // Only load in production
-      host: "https://analytics.kenshomedia.com/",
-      setCookieDomain: "*.kenshomedia.com",
-      trackerUrl: "js/", // defaults to matomo.php
-      srcUrl: "js/", // defaults to matomo.js
-      siteId: process.env.MATOMO_SITE_ID,
-      heartBeatTimer: 5,
-      disableCookies: true,
-      debug: false,
-    }),
-  ],
+  integrations: [tailwind(), svelte(), sitemap()],
   base: process.env.PUBLIC_SITE_ID === "gds" ? "/it" : "",
   trailingSlash: "always",
   vite: {
