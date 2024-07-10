bf8eaa49ab763e6d8094a7e1a14e08afffd934a8
Ikenna <ikenna@kenshomedia.com>
2024-07-10 11:48:48 +0100

    fixed home & custom page load error

--- qs/customPages.ts
+++ qs/customPages.ts
@@ -148,7 +148,6 @@ export const customPageQs = () => {
             },
           },
           homeFeaturedProviders: {
-            fields: ["title"],
             populate: {
               providers: {
                 fields: ["title", "slug"],
