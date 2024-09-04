14a8fa3d647d80758bbcc675e98237a7ac96a5b3
Tolumi Adamson <tolumi@kenshomedia.com>
2024-09-04 11:44:37 +0100

    Go URLs to Robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,7 +2,10 @@ import type { APIRoute } from "astro";
 
 const rootURL = () => {
     if (import.meta.env.ROOT_DOMAIN.includes("www")) {
-        return `Allow: /`
+      return `
+Allow: /
+Disallow: /go/*
+        `
     } else {
         return `Disallow: /`
     }
@@ -11,28 +14,29 @@ const rootURL = () => {
 const customURLS = () => {
         if (import.meta.env.SITE_ID === 'gds') {
             return `
-                Disallow: /it-IT/
-                Disallow: /es-MX/
-                Disallow: /es-CO/
-                Disallow: /en/
-                Disallow: /it/slots/
-                Disallow: /it/slot-games/
-                Disallow: /it/slot-casinos/snai-casino/p*
-                Disallow: /it/slot-casinos/betway/p*
-                Disallow: /it/slot-casinos/goldbet/p*
-                Disallow: /it/slot-casinos/888-casino/p*
-                Disallow: /it/slot-machine/piu-giocate/p*
-                Disallow: /it/casino-slots/voglia-di-vincere-casino/p*
-                Disallow: /it/casino-slots/betflag-casino/p*
-                Disallow: /it/casino-slots/admiral-yes-casino/p*
-                Disallow: /it/casino-slots/admiral-yes-casino/p*
-                Disallow: /it/slot-machine/migliori/p*
-                Disallow: /it/slot-software/p*
-                Disallow: /it/slot-casinos/slotyes/p*
-                Disallow: /it/giri-gratis/p*
-                Disallow: /it/casino/recensione/slot-machines/
-                Disallow: /it/software-slot-machine/slot-machines/
-            `
+Disallow: /it-IT/
+Disallow: /es-MX/
+Disallow: /es-CO/
+Disallow: /en/
+Disallow: /it/go/*
+Disallow: /it/slots/
+Disallow: /it/slot-games/
+Disallow: /it/slot-casinos/snai-casino/p*
+Disallow: /it/slot-casinos/betway/p*
+Disallow: /it/slot-casinos/goldbet/p*
+Disallow: /it/slot-casinos/888-casino/p*
+Disallow: /it/slot-machine/piu-giocate/p*
+Disallow: /it/casino-slots/voglia-di-vincere-casino/p*
+Disallow: /it/casino-slots/betflag-casino/p*
+Disallow: /it/casino-slots/admiral-yes-casino/p*
+Disallow: /it/casino-slots/admiral-yes-casino/p*
+Disallow: /it/slot-machine/migliori/p*
+Disallow: /it/slot-software/p*
+Disallow: /it/slot-casinos/slotyes/p*
+Disallow: /it/giri-gratis/p*
+Disallow: /it/casino/recensione/slot-machines/
+Disallow: /it/software-slot-machine/slot-machines/
+            `;
         }
 }
 
