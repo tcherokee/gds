db5323457edd472a43a0e31ccd0093340de2bee3
Tolumi Adamson <tolumi@kenshomedia.com>
2024-10-06 20:21:17 +0100

    Typescript Fixes and Middleware uncomment

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,83 +1,83 @@
-// // First and Third Party Plugins
-// import { defineMiddleware } from "astro:middleware";
+// First and Third Party Plugins
+import { defineMiddleware } from "astro:middleware";
 
-// import type { TRedirects } from "../../interfaces/common/types";
+import type { TRedirects } from "../../interfaces/common/types";
 
-// // Helpers
-// import fetchApi from "../../lib/strapi";
+// Helpers
+import fetchApi from "../../lib/strapi";
 
-// let cachedRedirects: TRedirects[] | null = null;
-// let cacheTimestamp: number = Date.now();
-// const CACHE_DURATION: number = 7 * 24 * 60 * 60 * 1000; // Cache Redirects for 7 Days
+let cachedRedirects: TRedirects[] | null = null;
+let cacheTimestamp: number = Date.now();
+const CACHE_DURATION: number = 7 * 24 * 60 * 60 * 1000; // Cache Redirects for 7 Days
 
 
-// // `context` and `next` are automatically typed
-// export const onRequest = defineMiddleware(async (context, next) => {
-//     try {
-//       // Redirection
-//       if (!cachedRedirects || Date.now() - cacheTimestamp > CACHE_DURATION) {
-//         const redirects = await fetchApi<TRedirects[]>({
-//           endpoint: "redirects",
-//           wrappedByKey: "data",
-//           query: `?pagination[pageSize]=1000`,
-//         });
+// `context` and `next` are automatically typed
+export const onRequest = defineMiddleware(async (context, next) => {
+    try {
+      // Redirection
+      if (!cachedRedirects || Date.now() - cacheTimestamp > CACHE_DURATION) {
+        const redirects = await fetchApi<TRedirects[]>({
+          endpoint: "redirects",
+          wrappedByKey: "data",
+          query: `?pagination[pageSize]=1000`,
+        });
           
-//           cachedRedirects = redirects
-//         cacheTimestamp = Date.now();
-//       }
+          cachedRedirects = redirects
+        cacheTimestamp = Date.now();
+      }
 
-//       // Loop through each item in the data array
-//         for (const item of cachedRedirects ?? []) {
-//           //Destructure Attributes
-//           const { redirectUrl, redirectTarget, redirectMethod } =
-//             item.attributes;
+      // Loop through each item in the data array
+        for (const item of cachedRedirects ?? []) {
+          //Destructure Attributes
+          const { redirectUrl, redirectTarget, redirectMethod } =
+            item.attributes;
 
-//           // Check if the current URL matches the redirectUrl
-//           if (context.url.pathname === redirectUrl) {
-//             // Determine the status code for the redirect
-//             const statusCode = redirectMethod === "permanent" ? 308 : 307;
+          // Check if the current URL matches the redirectUrl
+          if (context.url.pathname === redirectUrl) {
+            // Determine the status code for the redirect
+            const statusCode = redirectMethod === "permanent" ? 308 : 307;
 
-//             // Redirect to the target URL
-//             return Response.redirect(
-//               redirectTarget.includes("http://") ||
-//                 redirectTarget.includes("https://") ||
-//                 redirectTarget.includes("www")
-//                 ? redirectTarget
-//                 : `${context.url.origin}${redirectTarget}`,
-//               statusCode
-//             );
-//           }
-//         }
+            // Redirect to the target URL
+            return Response.redirect(
+              redirectTarget.includes("http://") ||
+                redirectTarget.includes("https://") ||
+                redirectTarget.includes("www")
+                ? redirectTarget
+                : `${context.url.origin}${redirectTarget}`,
+              statusCode
+            );
+          }
+        }
 
-//       // Authentication logic
-//       const authCookie = context.cookies.get("_token")?.value;
-//       const pathname = context.url.pathname;
-//       const dashboardIndex = pathname.split("/").indexOf("dashboard");
-//       const authenticationIndex = pathname.split("/").indexOf("authentication");
+      // Authentication logic
+      const authCookie = context.cookies.get("_token")?.value;
+      const pathname = context.url.pathname;
+      const dashboardIndex = pathname.split("/").indexOf("dashboard");
+      const authenticationIndex = pathname.split("/").indexOf("authentication");
 
-//       if (
-//         authCookie &&
-//         (authenticationIndex === 2 || authenticationIndex === 1)
-//       ) {
-//         return Response.redirect(
-//           new URL(`${import.meta.env.BASE_URL}dashboard/`, context.url),
-//           302
-//         );
-//       }
-//       if (!authCookie && (dashboardIndex === 2 || dashboardIndex === 1)) {
-//         return Response.redirect(
-//           new URL(
-//             `${import.meta.env.BASE_URL}authentication/login/`,
-//             context.url
-//           ),
-//           302
-//         );
-//       }
+      if (
+        authCookie &&
+        (authenticationIndex === 2 || authenticationIndex === 1)
+      ) {
+        return Response.redirect(
+          new URL(`${import.meta.env.BASE_URL}dashboard/`, context.url),
+          302
+        );
+      }
+      if (!authCookie && (dashboardIndex === 2 || dashboardIndex === 1)) {
+        return Response.redirect(
+          new URL(
+            `${import.meta.env.BASE_URL}authentication/login/`,
+            context.url
+          ),
+          302
+        );
+      }
 
-//       // return a Response or the result of calling `next()`
-//       return next();
-//     } catch (error) {
-//     console.error("Error during request handling:", error);
-//     return new Response("Internal Server Error", { status: 500 });
-//     }
-// });
+      // return a Response or the result of calling `next()`
+      return next();
+    } catch (error) {
+    console.error("Error during request handling:", error);
+    return new Response("Internal Server Error", { status: 500 });
+    }
+});
