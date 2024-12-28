2cd020d072ac15df50d738f5ee8a733ad043b6bb
Ikenna <ikenna@kenshomedia.com>
2024-12-28 11:54:06 +0100

    Testing ip

--- src/pages/api/ip.ts
+++ src/pages/api/ip.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ request }) => {
-  const clientIP = request.headers.get("x-forwarded-for") || "";
+  const clientIP =
+    request.headers.get("x-real-ip") ||
+    request.headers.get("x-forwarded-for") ||
+    "";
   return new Response(JSON.stringify({ ip: clientIP }), {
     headers: { "Content-Type": "application/json" },
   });
