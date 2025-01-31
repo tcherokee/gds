df05724201413d3b34d7662fefeaf2491a71c167
Tolumi Adamson <tolumi@kenshomedia.com>
2025-01-31 09:37:08 +0000

    Changing Headings

--- src/components/tournaments/tournaments.svelte
+++ src/components/tournaments/tournaments.svelte
@@ -52,7 +52,7 @@
 <section class="py-20 px-4">
   <div class="max-w-6xl mx-auto">
     <h2 class="text-4xl font-bold mb-12 text-center">
-      {liveTournaments[0]?.name || "Active Tournaments"}
+      Active Tournaments
     </h2>
     <div class="grid md:grid-cols-3 gap-8">
       {#if currentUserTournamentsLoader}
