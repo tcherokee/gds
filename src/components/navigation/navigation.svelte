787e2a0bb75ad31a627b4c74306d9f5d410ebcdb
Tolumi Adamson <tolumi@kenshomedia.com>
2025-03-28 18:17:38 +0000

    games h1 and removong menu h2

--- src/components/navigation/navigation.svelte
+++ src/components/navigation/navigation.svelte
@@ -91,12 +91,12 @@
           >
             <div class="px-4 sm:px-6">
               <div class="flex items-start justify-between">
-                <h2
+                <span
                   class="text-lg font-medium text-navbar-text"
                   id="slide-over-title"
                 >
                   Menu
-                </h2>
+              </span>
                 <div class="ml-3 flex h-7 items-center">
                   <button
                     type="button"
